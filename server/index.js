7018de934a169ee55ab59d70a02f67a71222dad5
Chad Sheets <cjsheets@gmail.com>
2017-02-19 22:27:40 -0800

    linting

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
 // Set default node environment to development
-var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if (env === 'development' || env === 'test') {
+if(env === 'development' || env === 'test') {
   process.env.DEBUG = 'app:*,api:*,config:*,webpack:*';
   process.env.APP_SECRET = '123123abcabc';
 }
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,55 +2,55 @@ import webpack from 'webpack';
 import path from 'path';
 
 export default {
-  devtool: 'inline-source-map',
-  entry: [
+  devtool : 'inline-source-map',
+  entry   : [
     'eventsource-polyfill', // necessary for hot reloading with IE
     'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
     path.resolve(__dirname, 'client/index')
   ],
-  target: 'web',
-  output: {
-    path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
-    publicPath: '/',
-    filename: 'bundle.js'
+  target : 'web',
+  output : {
+    path       : `${__dirname}/dist`, // Note: Physical files are only output by the production build task `npm run build`.
+    publicPath : '/',
+    filename   : 'bundle.js'
   },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'client')
+  devServer : {
+    contentBase : path.resolve(__dirname, 'client')
   },
-  plugins: [
+  plugins : [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin()
   ],
-  module: {
-    loaders: [
+  module : {
+    loaders : [
       {
-        test: /\.js$/,
-        include: path.join(__dirname, 'client'),
-        loaders: ['babel-loader']
+        test    : /\.js$/,
+        include : path.join(__dirname, 'client'),
+        loaders : ['babel-loader']
       }, {
-        test: /(\.css)$/,
-        loaders: ['style-loader', 'css-loader']
+        test    : /(\.css)$/,
+        loaders : ['style-loader', 'css-loader']
       }, {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file-loader'
-      }, { 
-        test: /\.png$/, 
-        loader: "url-loader?limit=100000" 
-      }, { 
-        test: /\.jpg$/, 
-        loader: "file-loader" 
+        test   : /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader : 'file-loader'
       }, {
-        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+        test   : /\.png$/,
+        loader : 'url-loader?limit=100000'
       }, {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
-        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+        test   : /\.jpg$/,
+        loader : 'file-loader'
       }, {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
-        loader: 'file-loader'
+        test   : /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader : 'url-loader?limit=10000&mimetype=application/font-woff'
       }, {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
-        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+        test   : /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader : 'url-loader?limit=10000&mimetype=application/octet-stream'
+      }, {
+        test   : /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader : 'file-loader'
+      }, {
+        test   : /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader : 'url-loader?limit=10000&mimetype=image/svg+xml'
       }
     ]
   }
